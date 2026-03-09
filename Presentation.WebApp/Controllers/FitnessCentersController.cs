904c5902b5189cb7bdbecfadc321be834f10e52f
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-03-09 09:36:36 +0100

    script added to layout

---
+++ Presentation.WebApp/Controllers/FitnessCentersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Attributes.MenuNavigation;
+
+namespace Presentation.WebApp.Controllers;
+
+public class FitnessCentersController : Controller
+{
+    [MenuItem("Fitness Centers", 1)]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
